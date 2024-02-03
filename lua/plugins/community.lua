da0feea7a84a9469f3e33ebc50de81ebc4bd84cb
limuy <limuyang202011@163.com>
2024-02-03 14:49:17 +0800

    s

--- lua/plugins/community.lua
+++ lua/plugins/community.lua
@@ -5,5 +5,6 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.pack.python" },
-  { import = "astrocommunity.pack.cpp" }
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.rust"}
 }
