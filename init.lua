23312338b20a0f3f3e5d85fbec3cc85ee573ed62
limuy <limuyang202011@163.com>
2024-06-02 07:47:55 +0800

    update

--- init.lua
+++ init.lua
@@ -29,6 +29,7 @@ require "polish"
 vim.cmd "noremap <leader>rd :RustLsp debuggables "
 vim.cmd "noremap <leader>rt :RustLsp runnables "
 vim.cmd "noremap <leader>rr :RustLsp renderDiagnostic<Enter>"
+vim.cmd "noremap <leader>rp :RustLsp rebuildProcMacros<Enter>"
 vim.cmd "noremap <leader>mp :MarkdownPreview<Enter>"
 vim.cmd "noremap <leader>pm :Telescope projects<Enter>"
 if vim.g.neovide then
