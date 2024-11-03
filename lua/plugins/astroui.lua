9f0d0904ed906c4253656f1a8a85b66b089e2c2e
limuy <limuyang202011@163.com>
2024-11-03 10:01:11 +0800

    update

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -9,9 +9,9 @@ return {
   ---@type AstroUIOpts
   opts = {
     -- change colorscheme
-    --     colorscheme = "astrodark",
+    colorscheme = "astrodark",
     -- colorscheme = "everforest",
-    colorscheme = "catppuccin",
+    -- colorscheme = "hybrid",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
